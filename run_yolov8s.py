5ad17a4ea78e44e49dbc390190608d0fdbe7c89d
minhquang5014 <minhquang5014@gmail.com>
2024-11-21 13:30:33 +0700

    first commitment

---
+++ run_yolov8s.py
@@ -0,0 +1,116 @@
+import torch
+import numpy as np
+import cv2
+from time import time
+from ultralytics import YOLO
+
+from supervision.draw.color import ColorPalette, Color
+from supervision import Detections, BoxAnnotator
+
+
+colors=[Color(r=255, g=64, b=64), Color(r=255, g=161, b=160)]
+class ObjectDetection:
+
+    def __init__(self, capture_index):
+       
+        self.capture_index = capture_index
+        
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Using Device: ", self.device)
+        
+        self.model = self.load_model()
+        
+        self.CLASS_NAMES_DICT = self.model.model.names
+    
+        self.box_annotator = BoxAnnotator(color=ColorPalette(colors=colors), thickness=3)
+    
+
+    def load_model(self):
+       
+        model = YOLO("yolov8n.pt")  # load a pretrained YOLOv8n model
+        model.fuse()
+    
+        return model
+
+
+    def predict(self, frame):
+       
+        results = self.model(frame)
+        
+        return results
+    
+
+    def plot_bboxes(self, results, frame):
+        
+        xyxys = []
+        confidences = []
+        class_ids = []
+        
+        # Extract detections for person class
+        for result in results[0]:
+            class_id = result.boxes.cls.cpu().numpy().astype(int)
+            
+            if class_id == 0:
+                
+                xyxys.append(result.boxes.xyxy.cpu().numpy())
+                confidences.append(result.boxes.conf.cpu().numpy())
+                class_ids.append(result.boxes.cls.cpu().numpy().astype(int))
+            
+        
+        # Setup detections for visualization
+        detections = Detections(
+                    xyxy=results[0].boxes.xyxy.cpu().numpy(),
+                    confidence=results[0].boxes.conf.cpu().numpy(),
+                    class_id=results[0].boxes.cls.cpu().numpy().astype(int),
+                    )
+    
+        # Format custom labels
+        self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
+        for _, confidence, class_id,
+        in detections]
+        print(self.labels)
+        
+        # Annotate and display frame
+        frame = self.box_annotator.annotate(frame=frame, detections=detections, labels=self.labels)
+        
+        return frame
+    
+    
+    
+    def __call__(self):
+
+        cap = cv2.VideoCapture(self.capture_index)
+        assert cap.isOpened()
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+      
+        while True:
+          
+            start_time = time()
+            
+            ret, frame = cap.read()
+            frame = cv2.flip(frame, 1)
+            if not ret:
+                break
+            
+            results = self.predict(frame)
+            frame = self.plot_bboxes(results, frame)
+            
+            end_time = time()
+            fps = 1/np.round(end_time - start_time, 2)
+             
+            cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
+            
+            cv2.imshow('YOLOv8 Detection', frame)
+ 
+            if cv2.waitKey(5) & 0xFF == 27:
+                
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+        
+        
+    
+detector = ObjectDetection(capture_index=0)
+detector()
